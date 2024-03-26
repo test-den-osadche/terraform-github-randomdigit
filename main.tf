d2c614356b6b7f6c4d62f9a57df23b656f39035f
test-den-osadche <164888207+test-den-osadche@users.noreply.github.com>
2024-03-26 11:48:20 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-module "randomdigit" {
-  source  = "git::https://github.com/test-den-osadche/terraform-github-randomdigit?ref=v1.0.0"
-  # other module configuration...
-}
-
 
 resource "random_integer" "random_number" {
   min = 1
